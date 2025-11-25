a49e61c4f1a631e301c3aad9cc3218976cff56c6
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-11-26 03:40:57 +0700

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,24 +17,12 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-        <div className="logo">AutoZone</div>
-      </header>
-
-      <main>
-        <section className="hero">
-          <h1>Get the Right Parts. Right Now</h1>
-          <p>Free Next Day Delivery on Eligible Orders</p>
-          <div className="search-bar">
-            <input type="text" placeholder='Enter vehicle or part number'></input>
-            <button>Search</button>
-          </div>
-        </section>
+      
 
         <button className="support-button" onClick={handleSupportClick}>
           Talk to an Agent!
         </button>
-      </main>
+      
 
       {showSupport && <LiveKitModal setShowSupport={setShowSupport}/>}
     </div>
